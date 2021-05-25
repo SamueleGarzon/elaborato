209f184c8a2125666ab366f6b9ea61b734355dbd
SamueleGarzon <84721192+SamueleGarzon@users.noreply.github.com>
2021-05-25 13:25:36 +0200

    Update JavaScript.js

--- Codice/JavaScript.js
+++ Codice/JavaScript.js
@@ -1,18 +1,28 @@
 var video=document.getElementById("Motore")
-
+var tempo=video.currentTime;
 function iniziaFerma(){
 	if (video.paused) {
+		tempo=video.currentTime;
+		document.getElementById("Motore").src="Immagini/GenBelloOn.mp4";
+		video.currentTime=tempo;
 		video.play();
 		document.getElementById("Interruttore").src="Immagini/On.jpg";
 	}else{ 
 		video.pause();
+		tempo=video.currentTime;
+		document.getElementById("Motore").src="Immagini/GenBelloOff.mp4";
+		video.currentTime=tempo;
 		document.getElementById("Interruttore").src="Immagini/Off.jpg";
 	}
 }
 
 video.onended=function(){
-		if(document.getElementById("Interruttore").src=="Immagini/Off.jpg")
-		video.play;
+		if(document.getElementById("Motore").src=="Immagini/Off.jpg"){
+			tempo=video.currentTime;
+			document.getElementById("Interruttore").src="Immagini/GenBelloOn.mp4";
+			video.currentTime=tempo;
+			video.play;
+		}
 }
 
 function test(){
